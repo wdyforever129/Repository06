90a627ee04213833ebd7c4c49ee2dea120cd5e7c
您的用户名 <>
2019-12-09 00:00:41 +0800

    cs122

--- springboot_resolver/src/main/java/com/evan/service/impl/ResolverServiceImpl.java
+++ springboot_resolver/src/main/java/com/evan/service/impl/ResolverServiceImpl.java
@@ -112,9 +112,7 @@ public class ResolverServiceImpl implements ResolverService {
         return sql;
     }
 
-    public String getCondition(String task_con){
-      return null;
-    }
+    
 
     public String getSql(String agg, List agg_as, List agg_column, String agg_con, List agg_group_by, List agg_order_by, Integer agg_size,
                          List return_columns, List table, String task_con, List task_order_by, Integer taske_size, String type) {
