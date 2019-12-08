4f42113d37dd6ec8f5d7a06144f48cf323196cbf
您的用户名 <>
2019-12-08 23:17:11 +0800

    添加es服务模块

--- springboot_resolver/src/main/java/com/evan/service/ElasticsearchService.java
+++ springboot_resolver/src/main/java/com/evan/service/ElasticsearchService.java
@@ -1,5 +1,8 @@
 package com.evan.service;
 
+import java.util.List;
+
 public interface ElasticsearchService {
-    String query();
+    String query(String agg, List agg_as, List agg_column, String agg_con, List agg_group_by, List agg_order_by, Integer agg_size,
+                 List return_columns, List table, String task_con, List task_order_by, Integer taske_size, String type);
 }
---
+++ springboot_resolver/src/main/java/com/evan/utils/FormatUtil.java
@@ -0,0 +1,12 @@
+package com.evan.utils;
+
+import java.util.Collections;
+
+public class FormatUtil {
+    /*public T[] getListToArray(){
+        return
+    }*/
+    // List<String> list1 = Arrays.asList(arr);
+    //Collections.addAll(list, array);
+    //Object[] objects = ls.toArray();
+}
